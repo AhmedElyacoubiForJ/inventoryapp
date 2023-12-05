d6171de2082727727c04225d8351452f593c41ae
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-12-05 15:49:33 +0100

    added welcome page

---
+++ src/main/java/edu/yacoubi/inventoryapp/AppController.java
@@ -0,0 +1,13 @@
+package edu.yacoubi.inventoryapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+    @GetMapping("")
+    String viewHomePage() {
+        return "index";
+    }
+}
