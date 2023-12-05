3d910b61aa32f70e3160a8544bfeac1b1d89c353
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-12-05 14:48:02 +0100

    Inventory project bootstrap w. Spring Boot Data One To Many demonstration and Thymeleaf

---
+++ src/test/java/edu/yacoubi/inventoryapp/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.yacoubi.inventoryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
