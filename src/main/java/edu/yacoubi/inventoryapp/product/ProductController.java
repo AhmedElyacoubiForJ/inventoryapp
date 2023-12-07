a8a97b48e720e929b48657cc0394fd47c19d7614
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-12-07 11:56:52 +0100

    added edit feature to product entity via product view

--- src/main/java/edu/yacoubi/inventoryapp/product/ProductController.java
+++ src/main/java/edu/yacoubi/inventoryapp/product/ProductController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -35,5 +36,19 @@ public class ProductController {
         List<Product> products = productRepository.findAll();
         model.addAttribute("listProducts", products);
         return "products";
+    }
+
+    @GetMapping("/products/edit/{id}")
+    public String showEditProductForm(
+            @PathVariable("id") Integer id,
+            Model model) {
+
+        Product product = productRepository.findById(id).get();
+        model.addAttribute("product", product);
+
+        List<Category> categories = categoryRepository.findAll();
+        model.addAttribute("listCategories", categories);
+
+        return "product_form";
     }
 }
